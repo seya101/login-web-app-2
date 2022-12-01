c46d9862ccef92de39a8538f4380b2620fa13ca8
Hosea Patricia Corpuz <Hosea.Corpuz@EntainGroup.com>
2022-12-01 16:51:42 +0800

    update info

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
         head: {
           charset: 'utf-16',
           viewport: 'width=500, initial-scale=1',
-          title: 'Login | Entain Creative KPI',
+          title: 'Login Creative UI',
           meta: [
             // <meta name="description" content="My amazing site">
             { name: 'description', content: 'My amazing site.' }
